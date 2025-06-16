21507026316404f752131c1101aa009d8266e720
Bart Thate <bthate@dds.nl>
2025-06-16 19:06:02 +0200

    add tests

---
+++ tests/test_handler.py
@@ -0,0 +1,27 @@
+# This file is placed in the Public Domain.
+
+
+"handler"
+
+
+import unittest
+
+
+from nixts.event   import Event
+from nixts.handler import Handler
+from nixts.modules import command
+
+
+hdl = Handler()
+hdl.register("command", command)
+hdl.start()
+
+
+class TestHandler(unittest.TestCase):
+
+    def test_loop(self):
+        e = Event()
+        e.txt = "dbg"
+        hdl.put(e)
+        e.wait()
+        self.assertTrue(True)
---
+++ tests/test_interface.py
@@ -0,0 +1,103 @@
+# This file is placed in the Public Domain.
+# ruff: noqa: F403,F405
+
+
+"interface"
+
+
+import logging
+import sys
+import unittest
+
+
+import nixts
+import nixts.client
+import nixts.errors
+import nixts.event
+import nixts.find
+import nixts.fleet
+import nixts.handler
+import nixts.object
+import nixts.path
+import nixts.persist
+import nixts.thread
+import nixts.timer
+
+
+from nixts.object import *
+
+
+PACKAGE = [
+    'client',
+    'errors',
+    'event',
+    'find',
+    'fleet',
+    'handler',
+    'object',
+    'path',
+    'persist',
+    'thread',
+    'timer'
+]
+
+
+METHODS = [
+    '__class__',
+    '__delattr__',
+    '__dict__',
+    '__dir__',
+    '__doc__',
+    '__eq__',
+    '__format__',
+    '__ge__',
+    '__getattribute__',
+    '__getstate__',
+    '__gt__',
+    '__hash__',
+    '__init__',
+    '__init_subclass__',
+    '__le__',
+    '__len__',
+    '__lt__',
+    '__module__',
+    '__ne__',
+    '__new__',
+    '__reduce__',
+    '__reduce_ex__',
+    '__repr__',
+    '__setattr__',
+    '__sizeof__',
+    '__str__',
+    '__subclasshook__',
+    '__weakref__'
+]
+
+
+class TestInterface(unittest.TestCase):
+
+    def test_package(self):
+        okd = True
+        for mod in PACKAGE:
+            mod1 = getattr(nixts, mod, None)
+            if not mod1:
+                okd = False
+                print(mod)
+                break
+        self.assertTrue(okd)
+
+    def test_objects(self):
+        okd = True
+        obj = Object()
+        dirr = dir(obj)
+        for meth in METHODS:
+            if meth not in dirr:
+                okd = False
+                print(f"{meth} not found")
+        self.assertTrue(okd)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stderr)
+    logging.getLogger("SomeTest.testSomething").setLevel(logging.DEBUG)
+    unittest.main()
